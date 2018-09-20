610d1d80814206f394f783bf63aa2993e2d887c9
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-09-20 18:14:32 -0300

    Minhas Cenas implementado

---
+++ sistema/minhasCenas.php
@@ -0,0 +1,41 @@
+<?php session_start(); ?>
+<?php include_once 'php/_header.php'; ?>
+<?php include_once 'php/banner.php'; ?>
+<?php include_once 'php/menu.php'; ?>
+
+<div class="conteudo">
+    <h1>Minhas Cenas</h1>
+    <?php
+        $usuarioEmail = $_SESSION["usuarioEmail"];
+        include 'php/_dbconnect.php';
+        $sql = "SELECT C.stNome AS cNome, C.stMundo AS cMundoId, C.stId AS cId, "
+                . "C.dtData AS cData, M.stNome AS mNome, P.stNome AS pNome "
+                . "FROM tbCenas C INNER JOIN tbMundos M INNER JOIN tbPersonagens P "
+                . "ON C.stMundo = M.stId && C.stCreator = P.stId "
+                . "WHERE P.stDono='$usuarioEmail' ORDER BY C.dtData";
+        $query = $con->query($sql);
+        if(!$query){
+            echo "Erro no query: ". mysqli_error($con);
+            mysqli_close($con);
+            die();
+        }
+        if($query->num_rows>0){
+            while($dados = $query->fetch_array(MYSQLI_ASSOC)){
+                $cenaId = $dados["cId"];
+                $cenaMundoId = $dados["cMundoId"];
+                $cenaNome = $dados["cNome"];
+                $cenaMundo = $dados["mNome"];
+                $cenaData = $dados["cData"];
+                $cenaPersonagem = $dados["pNome"];
+                echo "<div class='cenaBox'><a href='cena.php?mundo=$cenaMundoId&id=$cenaId'>"
+                        . "<h2>$cenaNome</h2></a>"
+                . "<h3>Em $cenaMundo criada em $cenaData por $cenaPersonagem</h3></div>";
+            }
+        }else{
+            echo 'Você não possui cenas em mundo algum.';
+        }
+        mysqli_close($con);
+    ?>
+</div>
+
+<?php include_once 'php/_footer.php'; ?>
--- sistema/php/menu.php
+++ sistema/php/menu.php
@@ -1,7 +1,9 @@
 <div id="menu">
     <ul>
         <li><a href="selecionaMundo.php">Início</a></li>
+        <li><a href="notifs.php">Notificações</a></li>
         <li><a href="meusPersonagens.php">Meus Personagens</a></li>
+        <li><a href="minhasCenas.php">Minhas Cenas</a></li>
         <li><a href="meusMundos.php">Meus Mundos</a></li>
         <li><a href="conta.php">Configurações da Conta</a></li>
         <li><a href="php/sessionDestroyer.php">Logout</a></li>
